dd8b84625d3c46aaa98785976474eac52624677d
ChengYong Tham <25361260+cytham@users.noreply.github.com>
2023-03-27 00:35:19 +0800

    Increased multi-mapping from 4 to 6

--- nanovar/nv_detect_algo.py
+++ nanovar/nv_detect_algo.py
@@ -550,7 +550,7 @@ def reduce_multi_map(subdata):
             sortdictlen[line] = int(line.split('\t')[6])
         # Sorting according to alignment length
         subdata2 = [key for (key, value) in sorted(sortdictlen.items(), key=lambda x: x[1], reverse=True)]
-        subdata3 = subdata2[0:6]  # Selecting top 4 longest alignments
+        subdata3 = subdata2[0:6]  # Selecting top 6 longest alignments
         # Sort back according to query start
         sortdict = {}
         for e in subdata3:
