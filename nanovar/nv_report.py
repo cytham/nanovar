7d9fd15b2ae664b3c3f7764802a3667e65cb1da1
ChengYong Tham <25361260+cytham@users.noreply.github.com>
2023-04-03 15:18:45 +0800

    Update nv_report.py

--- nanovar/nv_report.py
+++ nanovar/nv_report.py
@@ -563,7 +563,7 @@ def create_html(data, fwd, wk_dir, vcf_path, timenow, read_name, read_path, ref_
     """
     html.write(row)
     html.close()
-    packed_html = htmlark.convert_page(html)
+    packed_html = htmlark.convert_page(os.path.join(wk_dir, '%s.nanovar.pass.report-tmp.html' % read_name))
     html_final = open(os.path.join(wk_dir, '%s.nanovar.pass.report.html' % read_name), 'w')
     html_final.write(packed_html)
     html_final.close()
