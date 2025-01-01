bff6cdbe7d5287c281e6e5d1af4bab5b10d758a7
Cheng Yong Tham <25361260+cytham@users.noreply.github.com>
2025-01-02 01:03:34 +0800

    Update nv_report.py
    Write HTML by soup.prettify

--- src/nanovar/nv_report.py
+++ src/nanovar/nv_report.py
@@ -602,11 +602,11 @@ def create_html(data, fwd, wk_dir, vcf_path, timenow, read_name, read_path, ref_
             mtype = mtype + ';base64'
             en_data = base64.b64encode(data).decode()
             tag['src'] = "data:{},{}".format(mtype, en_data)
-    packed_html = str(soup)
+    # packed_html = str(soup)
     with open(os.path.join(wk_dir, f"{read_name}.nanovar.pass.report.html"), "w", encoding = 'utf-8') as file: 
         file.write(str(soup.prettify()))
     # packed_html = htmlark.convert_page(os.path.join(wk_dir, '%s.nanovar.pass.report-tmp.html' % read_name), ignore_errors=True)
-    html_final = open(os.path.join(wk_dir, '%s.nanovar.pass.report.html' % read_name), 'w')
-    _ = html_final.write(packed_html)
-    html_final.close()
+    # html_final = open(os.path.join(wk_dir, '%s.nanovar.pass.report.html' % read_name), 'w')
+    # _ = html_final.write(packed_html)
+    # html_final.close()
     os.remove(os.path.join(wk_dir, '%s.nanovar.pass.report-tmp.html' % read_name))
